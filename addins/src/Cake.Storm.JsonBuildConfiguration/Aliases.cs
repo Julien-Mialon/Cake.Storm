27debdfc127b760d563e07907e21505f84de1606
Julien Mialon <julien.mialon@ideine.fr>
2016-12-07 18:09:18 +0100

    start working on configuration

---
+++ addins/src/Cake.Storm.JsonBuildConfiguration/Aliases.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.Core.Diagnostics;
+using Cake.Core.IO;
+using Cake.Storm.JsonBuildConfiguration.Models;
+using Newtonsoft.Json;
+
+namespace Cake.Storm.JsonBuildConfiguration
+{
+	[CakeAliasCategory("Cake.Storm.JsonBuildConfiguration")]
+	public static class Aliases
+	{
+		[CakeMethodAlias]
+		[CakeNamespaceImport("Cake.Storm.JsonBuildConfiguration.Models")]
+		public static Configuration ReadJsonConfigurationFile(this ICakeContext context, FilePath jsonFile)
+		{
+			string fileContent = null;
+			try
+			{
+				fileContent = File.ReadAllText(jsonFile.FullPath);
+			}
+			catch (Exception ex)
+			{
+				context.Log.Write(Verbosity.Quiet, LogLevel.Error, $"Json file not found {jsonFile.FullPath}");
+			}
+
+			try
+			{
+				return JsonConvert.DeserializeObject<Configuration>();
+			}
+			catch (Exception ex)
+			{
+
+			}
+			jsonFile.FullPath
+			        
+		}
+	}
+}
