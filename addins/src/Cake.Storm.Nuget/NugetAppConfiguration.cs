0acdf26c05d37339af90b70f1aec3b3745825056
Julien Mialon <julien.mialon@ideine.fr>
2016-12-11 19:57:00 +0100

    Add nuget addin + clean local tests directory

---
+++ addins/src/Cake.Storm.Nuget/NugetAppConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Cake.Storm.Nuget
+{
+	public class NugetAppConfiguration
+	{
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("id")]
+		public string Id { get; set; }
+
+		[JsonProperty("version")]
+		public string Version { get; set; }
+
+		[JsonProperty("author")]
+		public string Author { get; set; }
+
+		[JsonProperty("releaseNoteFile")]
+		public string ReleaseNoteFile { get; set; }
+
+		[JsonProperty("nuspecFile")]
+		public string NuspecFile { get; set; }
+
+		[JsonProperty("path")]
+		public string Path { get; set; }
+
+		[JsonProperty("packOutput")]
+		public string PackOutput { get; set; }
+
+		[JsonProperty("pushSource")]
+		public string PushSource { get; set; }
+	}
+}
