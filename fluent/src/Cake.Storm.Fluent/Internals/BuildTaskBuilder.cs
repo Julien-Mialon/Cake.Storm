3b171840a4491677f1afc8fe9371b1b0ab78baa5
Julien Mialon <mialon.julien@gmail.com>
2017-09-30 17:43:53 +0200

    Add validation on target and platforms name links from targets and application #6

--- fluent/src/Cake.Storm.Fluent/Internals/BuildTaskBuilder.cs
+++ fluent/src/Cake.Storm.Fluent/Internals/BuildTaskBuilder.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Cake.Core;
 using Cake.Core.Diagnostics;
@@ -48,7 +49,7 @@ namespace Cake.Storm.Fluent.Internals
 
 		public void Build()
 		{
-			List<Target> targets = MergeTargets();
+			List<Target> targets = MergeTargets(_parameters);
 
 			//generate application-target-platform
 			foreach (Target target in targets)
@@ -175,7 +176,7 @@ namespace Cake.Storm.Fluent.Internals
 
 		public void Help()
 		{
-			List<Target> targets = MergeTargets();
+			List<Target> targets = MergeTargets(_parameters);
 
 			ICakeLog logger = _parameters.Context.CakeContext.Log;
 
@@ -245,6 +246,196 @@ namespace Cake.Storm.Fluent.Internals
 			deployTask.Does(() => { });
 		}
 
+		private static void ValidateParameters(BuilderParameters parameters)
+		{
+			List<string> logs = new List<string>();
+			if (parameters.PlatformsConfiguration.Count == 0)
+			{
+				logs.Add("Error: No platforms defined");
+			}
+			if (parameters.TargetsConfiguration.Count == 0)
+			{
+				logs.Add("Error: No targets defined");
+			}
+			if (parameters.ApplicationsConfiguration.Count == 0)
+			{
+				logs.Add("Error: No applications defined");
+			}
+
+			logs.AddRange(
+				parameters.PlatformsConfiguration.Keys
+					.Where(platformName => !ValidateNames(platformName))
+					.Select(platformName => $"Error: platform {platformName} has invalid name, only [A-Za-z0-9_-] allowed"));
+
+			logs.AddRange(
+				parameters.TargetsConfiguration.Keys
+					.Where(targetName => !ValidateNames(targetName))
+					.Select(targetName => $"Error: target {targetName} has invalid name, only [A-Za-z0-9_-] allowed"));
+
+			logs.AddRange(
+				parameters.ApplicationsConfiguration.Keys
+					.Where(applicationName => !ValidateNames(applicationName))
+					.Select(applicationName => $"Error: application {applicationName} has invalid name, only [A-Za-z0-9_-] allowed"));
+
+			//check if all platform used in targets are defined in global list
+			logs.AddRange(
+				parameters.TargetsConfiguration.SelectMany(target => 
+					target.Value.Platforms.Keys
+						.Where(platformName => !parameters.PlatformsConfiguration.ContainsKey(platformName))
+						.Select(platformName => $"Error: platform {platformName} used in target {target.Key} does not exists in platforms list")
+					)
+				);
+			
+			//check if all targets used in application are defined in global list 
+			logs.AddRange(
+				parameters.ApplicationsConfiguration.SelectMany(application => 
+					application.Value.Targets.Keys
+						.Where(targetName => !parameters.TargetsConfiguration.ContainsKey(targetName))
+						.Select(targetName => $"Error: target {targetName} used in application {application.Key} does not exists in targets list")
+					)
+				);
+			
+			//check if all platforms used in targets in application are defined for this targets
+			logs.AddRange(
+				parameters.ApplicationsConfiguration.SelectMany(application => 
+					application.Value.Targets
+						//check if all platforms used are defined in global list
+						.SelectMany(target => 
+							target.Value.Platforms.Keys
+								.Where(platformName => !parameters.PlatformsConfiguration.ContainsKey(platformName))
+								.Select(platformName => $"Error: platform {platformName} used in application {application.Key} and target {target.Key} does not exists in platforms list")
+						)
+						//check if all platforms used are also defined in the corresponding target platforms list
+						.Concat(application.Value.Targets
+							.Where(target => parameters.TargetsConfiguration.ContainsKey(target.Key))
+							.Select(target => Tuple.Create(target, parameters.TargetsConfiguration[target.Key]))
+							.Where(targets => targets.Item2.Platforms.Count > 0)
+							.SelectMany(targets => 
+								targets.Item1.Value.Platforms.Keys
+									.Where(platformName => !targets.Item2.Platforms.ContainsKey(platformName))
+									.Select(platformName => $"Error: platform {platformName} used in application {application.Key} and target {targets.Item1.Key} does not exists in the target {targets.Item1.Key} platforms list")
+							)
+						)
+					)
+				);
+			
+			if (logs.Count > 0)
+			{
+				parameters.Context.CakeContext.LogAndThrow(string.Join(Environment.NewLine, logs));
+			}
+		}
+
+		private static bool ValidateNames(string name)
+		{
+			return !string.IsNullOrWhiteSpace(name) &&
+			       name.All(c => char.IsLetterOrDigit(c) || c == '-' || c == '_');
+		}
+
+		private static List<Target> MergeTargets(BuilderParameters parameters)
+		{
+			ValidateParameters(parameters);
+			List<Target> result = new List<Target>();
+
+			foreach (KeyValuePair<string, IApplicationConfiguration> applicationItem in parameters.ApplicationsConfiguration)
+			{
+				IApplicationConfiguration applicationConfiguration = applicationItem.Value;
+				
+				IReadOnlyDictionary<string, ITargetConfiguration> applicationTargets = null;
+				IReadOnlyDictionary<string, ITargetConfiguration> targets;
+				
+				if (applicationConfiguration.Targets.Count > 0) // does the application define specific targets or not ?
+				{
+					applicationTargets = applicationConfiguration.Targets;
+					targets = FilterTargets(parameters.TargetsConfiguration, applicationTargets); //filter global list of targets to be restricted to the ones accepted by the application
+				}
+				else //if not, use all defined target in the global configuration
+				{
+					targets = parameters.TargetsConfiguration;
+				}
+				
+				//on each targets 
+				foreach (KeyValuePair<string, ITargetConfiguration> targetItem in targets)
+				{
+					string targetName = targetItem.Key;
+					ITargetConfiguration targetConfiguration = targetItem.Value;
+					ITargetConfiguration applicationTargetConfiguration = applicationTargets?[targetName];
+					
+					IReadOnlyDictionary<string, IPlatformConfiguration> applicationTargetPlatforms = null;
+					IReadOnlyDictionary<string, IPlatformConfiguration> targetPlatforms = null;
+					IReadOnlyDictionary<string, IPlatformConfiguration> platforms;
+
+					if (applicationTargetConfiguration != null && applicationTargetConfiguration.Platforms.Count > 0)
+					{
+						applicationTargetPlatforms = applicationTargetConfiguration.Platforms;
+						if (targetConfiguration.Platforms.Count > 0)
+						{
+							targetPlatforms = FilterPlatforms(targetConfiguration.Platforms, applicationTargetPlatforms); //filter list of platforms in target to the ones defined by the application
+						}
+						platforms = FilterPlatforms(parameters.PlatformsConfiguration, applicationTargetPlatforms); //filter global list of platforms to the ones defined in the application
+					}
+					else if(targetConfiguration.Platforms.Count > 0)
+					{
+						targetPlatforms = targetConfiguration.Platforms;
+						platforms = FilterPlatforms(parameters.PlatformsConfiguration, targetPlatforms); //filter global list of platforms to be restricted to the ones accepted by the target
+					}
+					else
+					{
+						platforms = parameters.PlatformsConfiguration;
+					}
+					
+					foreach (KeyValuePair<string, IPlatformConfiguration> platformItem in platforms)
+					{
+						string platformName = platformItem.Key;
+						IPlatformConfiguration platformConfiguration = platformItem.Value;
+						IPlatformConfiguration targetPlatformConfiguration = targetPlatforms?[platformName];
+						IPlatformConfiguration applicationTargetPlatformConfiguration = applicationTargetPlatforms?[platformName];
+						
+						IConfiguration rootLevel = parameters.RootConfiguration;
+						IConfiguration platformLevel = platformConfiguration;
+						IConfiguration targetLevel = targetConfiguration;
+						IConfiguration applicationLevel = applicationConfiguration;
+						
+						// merge platform specific detail in target level
+						if (targetPlatformConfiguration != null)
+						{
+							targetLevel = targetLevel.Merge(targetPlatformConfiguration);
+						}
+						
+						// merge target and platform specific detail in application level
+						if (applicationTargetConfiguration != null)
+						{
+							IConfiguration applicationTargetLevel = applicationTargetConfiguration;
+							if (applicationTargetPlatformConfiguration != null)
+							{
+								applicationTargetLevel = applicationTargetLevel.Merge(applicationTargetPlatformConfiguration);
+							}
+							applicationLevel = applicationLevel.Merge(applicationTargetLevel);
+						}
+
+						IConfiguration configuration = rootLevel.Merge(platformLevel.Merge(targetLevel.Merge(applicationLevel)));
+						result.Add(new Target(applicationItem.Key, targetItem.Key, platformItem.Key, configuration));
+					}
+				}
+			}
+
+			return result;
+		}
+
+		private static IReadOnlyDictionary<string, ITargetConfiguration> FilterTargets(IReadOnlyDictionary<string, ITargetConfiguration> globalTargets, IReadOnlyDictionary<string, ITargetConfiguration> applicationTargets)
+		{
+			return applicationTargets.Keys
+				.Select(targetName => Tuple.Create(targetName, globalTargets[targetName]))
+				.ToDictionary(x => x.Item1, x => x.Item2);
+		}
+		
+		private static IReadOnlyDictionary<string, IPlatformConfiguration> FilterPlatforms(IReadOnlyDictionary<string, IPlatformConfiguration> globalPlatforms, IReadOnlyDictionary<string, IPlatformConfiguration> targetPlatforms)
+		{
+			return targetPlatforms.Keys
+				.Select(platformName => Tuple.Create(platformName, globalPlatforms[platformName]))
+				.ToDictionary(x => x.Item1, x => x.Item2);
+		}
+
+		/*
 		private List<Target> MergeTargets()
 		{
 			List<Target> targets = new List<Target>();
@@ -281,12 +472,28 @@ namespace Cake.Storm.Fluent.Internals
 					foreach (KeyValuePair<string, IPlatformConfiguration> platformItem in platformsForTarget)
 					{
 						IConfiguration rootLevel = _parameters.RootConfiguration;
+
+						if (!_parameters.PlatformsConfiguration.ContainsKey(platformItem.Key))
+						{
+							_parameters.Context.CakeContext.LogAndThrow($"Platform with name {platformItem.Key} does not exists");
+						}
+						
 						IConfiguration platformLevel = _parameters.PlatformsConfiguration[platformItem.Key];
+
+						if (!_parameters.TargetsConfiguration.ContainsKey(targetItem.Key))
+						{
+							_parameters.Context.CakeContext.LogAndThrow($"Target with name {targetItem.Key} does not exists");
+						}
 						ITargetConfiguration targetConfiguration = _parameters.TargetsConfiguration[targetItem.Key];
 						IConfiguration targetLevel = targetConfiguration;
 						if (targetConfiguration.Platforms.TryGetValue(platformItem.Key, out IPlatformConfiguration targetPlatformConfiguration))
 						{
 							targetLevel = targetConfiguration.Merge(targetPlatformConfiguration);
+						}
+						else //at least validate if the platform defined in the target configuration really exists
+						{
+							
+							
 						}
 						//merge application sub configuration if needed (not from list of all)
 						IConfiguration applicationLevel = null;
@@ -309,5 +516,6 @@ namespace Cake.Storm.Fluent.Internals
 			}
 			return targets;
 		}
+		*/
 	}
 }
