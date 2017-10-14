a5bb015c4a3db5cbde11c5dc9bb6a1390ac185cf
Julien Mialon <mialon.julien@gmail.com>
2017-10-14 20:14:48 +0200

    Add cache for msbuild step #23

--- fluent/src/Cake.Storm.Fluent/Common/Steps/MSBuildSolutionStep.cs
+++ fluent/src/Cake.Storm.Fluent/Common/Steps/MSBuildSolutionStep.cs
@@ -1,4 +1,5 @@
-﻿using Cake.Common.Tools.MSBuild;
+﻿using System.Linq;
+using Cake.Common.Tools.MSBuild;
 using Cake.Storm.Fluent.Interfaces;
 using Cake.Storm.Fluent.InternalExtensions;
 using Cake.Storm.Fluent.Steps;
@@ -6,7 +7,7 @@ using Cake.Storm.Fluent.Steps;
 namespace Cake.Storm.Fluent.Common.Steps
 {
 	[BuildStep]
-	public class MSBuildSolutionStep : IStep
+	public class MSBuildSolutionStep : ICacheableStep
 	{
 		public void Execute(IConfiguration configuration, StepType currentStep)
 		{
@@ -14,6 +15,17 @@ namespace Cake.Storm.Fluent.Common.Steps
 			configuration.FileExistsOrThrow(solutionPath);
 
 			configuration.Context.CakeContext.MSBuild(solutionPath, configuration.ApplyBuildParameters);
+		}
+
+		public string GetCacheId(IConfiguration configuration, StepType currentStep)
+		{
+			string solutionPath = configuration.GetSolutionPath();
+			
+			MSBuildSettings settings = new MSBuildSettings();
+			configuration.ApplyBuildParameters(settings);
+
+			return $"{solutionPath}_{settings.Configuration}_{settings.MSBuildPlatform}_{settings.PlatformTarget}_{settings.ToolPath?.FullPath}_{settings.WorkingDirectory?.FullPath}"
+			       + $"{settings.ToolVersion}_{string.Join(";", settings.Targets)}_{string.Join(";", settings.Properties.Select(x => $"{x.Key}={string.Join(",", x.Value)}"))}";
 		}
 	}
 }
